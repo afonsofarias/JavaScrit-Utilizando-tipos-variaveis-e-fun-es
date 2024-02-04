ff1e33e57a1b1aab9c5b9dbc1941ce415aa19596
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-04 10:55:03 -0300

    first commit

---
+++ syntaxerror.js
@@ -0,0 +1,14 @@
+let numero = 10;
+let nome = "Alice;
+
+if (numero === 10) {
+  console.log("O número é 10.");
+} else {
+  console.log("O número não é 10.");
+}
+
+if (nome === "Alice") {
+  console.log("Boas vindas, Alice!");
+} else {
+  console.log("Você não é a Alice.");
+}
---
+++ variaveis.js
@@ -0,0 +1,6 @@
+const estudante = 'Caroline';
+let professora;
+
+professora = 'Ana';
+
+console.log(estudante, professora);
