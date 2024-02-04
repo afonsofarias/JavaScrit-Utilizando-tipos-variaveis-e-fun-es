fe79e063a21d8c8a1c7bcd579b1294097d4d6729
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-04 13:54:16 -0300

    upload espressao-funcao.js

---
+++ expressao-funcao.js
@@ -0,0 +1,14 @@
+function estudanteReprovou (notaFinal, faltas) {
+    if(notaFinal < 7 && faltas > 4) {
+        console.log('reprovado');
+        return true;
+        
+    } else{
+        console.log('não está reprovado');
+        return false;
+        
+    }
+}
+
+console.log(estudanteReprovou(6, 5));
+console.log(estudanteReprovou(10, 2));
